4da9ea7509d03fb0c9ca5e22ad838fbf5cde01ce
vgabriel10 <vittorgabriell420@gmail.com>
2025-03-25 16:03:12 -0300

    Criando tela de cadastro de pacientes

---
+++ ProConsulta/Components/Pages/Pacientes/Create.razor.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using MudBlazor;
+using ProConsulta.Models;
+using ProConsulta.Repositories.Pacientes;
+
+namespace ProConsulta.Components.Pages.Pacientes
+{
+    public class CreatePacientePage : ComponentBase
+    {
+        [Inject]
+        public IPacienteRepository repository { get; set; } = null!;
+
+        [Inject]
+        public ISnackbar Snackbar { get; set; } = null!;
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; } = null!;
+
+        public PacienteInputModel InputModel { get; set; } = new PacienteInputModel();
+
+        public DateTime? DataNascimento { get; set; } = DateTime.Today;
+
+        public async Task OnValidSubimitAsync(EditContext editContext)
+        {
+            try
+            {
+                if (editContext.Model is PacienteInputModel model)
+                {
+                    var paciente = new Paciente
+                    {
+                        Nome = model.Nome,
+                        Documento = model.Documento,
+                        Celular = model.Celular,
+                        Email = model.Email,
+                        DataNascimento = DataNascimento.Value
+                    };
+
+                    await repository.AddAsync(paciente);
+                    Snackbar.Add("Paciente cadastrado com sucesso!", Severity.Success);
+                    NavigationManager.NavigateTo("/pacientes");
+                }
+            }
+            catch (Exception ex)
+            {
+                Snackbar.Add($"{ex.Message}", Severity.Error);
+            }
+        }
+
+    }
+}
